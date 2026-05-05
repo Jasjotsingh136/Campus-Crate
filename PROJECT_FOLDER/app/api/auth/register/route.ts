6a9b305984ffb0b12992b01c61b89d4cce56ddbb
Jasjotsingh136 <140630036+Jasjotsingh136@users.noreply.github.com>
2026-05-06 01:53:33 +0530

    Update route.ts

--- PROJECT_FOLDER/app/api/auth/register/route.ts
+++ PROJECT_FOLDER/app/api/auth/register/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 
+export const dynamic = "force-dynamic"
+export const runtime = "nodejs"
 export async function POST(req: Request) {
   try {
     const { name, email, password, hostelBlock } = await req.json();
